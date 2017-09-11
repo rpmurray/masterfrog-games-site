74e8476874d04ced119b885d6fafa18c2aee7f61
Robert Murray <rmurray@elance-odesk.com>
2017-09-11 11:00:01 -0700

    minor tweaks to PHP test

--- php/index.php
+++ php/index.php
@@ -1,7 +1,14 @@
 <html>
 <body>
     <?php
-    echo "PHP v5.5.9 is operational on frog01.masterfrog.info at " . date("g:i:s A") . " on " . date("l, F d, Y") . "!";
+    // if test not set, redirect to html site
+    if (empty($_GET['test'])) {
+        header("Location: /html");
+        exit();
+    }
+
+    // otherwise execute PHP test
+	echo "PHP v" . explode('-', PHP_VERSION)[0] . " is operational on " . $_SERVER['SERVER_NAME'] . " at " . date("h:i:s A e") . " on " . date("l, F d, Y") . "!";
     ?>
 </body>
 </html>
